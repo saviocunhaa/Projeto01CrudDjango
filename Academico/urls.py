47244d6f8139fbffd9250b9470b65dbde08ee978
saviocunhaa <saviocunha.01@hotmail.com>
2023-04-19 16:51:44 -0300

    ajustes

--- Academico/urls.py
+++ Academico/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('registrarCurso/', views.registrarCursos)
 ]
--- Academico/views.py
+++ Academico/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
 from .models import Curso
 
 # Create your views here.
@@ -7,3 +8,12 @@ from .models import Curso
 def home(request):
     cursoListados = Curso.objects.all()
     return render(request, "gestaoCursos.html", {"cursos": cursoListados})
+
+
+def registrarCursos(request):
+    codigo = request.POST['txtCodigo']
+    nome = request.POST['txtNome']
+    creditos = request.POST['txtCredito']
+
+    curso = Curso.objects.create(codigo=codigo, nome=nome, creditos=creditos)
+    return redirect('/')
